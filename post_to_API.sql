b30363ab53efe1309013151cb75d4e0ede62add0
Abdul Wasay <77259168+Ebbdul@users.noreply.github.com>
2023-10-26 10:25:30 +0500

    Update post_to_API.sql

--- post_to_API.sql
+++ post_to_API.sql
@@ -1,39 +1,28 @@
-DELIMITER //
+-- PROCEDURE: public.post_data_to_api12(character varying, character varying)
 
-CREATE PROCEDURE PostDataToAPI6(
-  IN apiUrl VARCHAR(255),
-  IN postData VARCHAR(255),
-  OUT result VARCHAR(500)
-)
+-- DROP PROCEDURE IF EXISTS public.post_data_to_api12(character varying, character varying);
+
+CREATE OR REPLACE PROCEDURE public.post_data_to_api12(
+	IN input1 character varying,
+	IN input2 character varying)
+LANGUAGE 'plpgsql'
+AS $BODY$
+DECLARE
+    data JSONB;
 BEGIN
-  DECLARE curlCmd VARCHAR(500);
-  DECLARE cmdOutput VARCHAR(500);
+    -- Construct the data payload
+    data := jsonb_build_object('input1', input1, 'input2', input2);
 
-  SET curlCmd = CONCAT('curl -X POST -H "Content-Type: application/json" -d "', postData, '" "', apiUrl, '"');
-  
-  -- Execute the curl command using sys_exec and capture the output
-  SET cmdOutput = sys_exec(curlCmd);
-
-  SET result = cmdOutput;
-END //
-
-DELIMITER ;
-CALL PostDataToAPI6('http://127.0.0.1:5000/api/user', '{\\"name\\": \\"John\\", \\"age\\": 25}', @result);
+    -- Call the Python API with the data payload
+    PERFORM pg_notify('api_notification', data::TEXT);
+END;
+$BODY$;
+ALTER PROCEDURE public.post_data_to_api12(character varying, character varying)
+    OWNER TO postgres;
 
 
-DELIMITER //
-create procedure test2(IN apiUrl VARCHAR(255), IN postData VARCHAR(255))
-Begin 
-  SET @curlCmd = CONCAT('curl -X POST -H "Content-Type: application/json" -d "', postData, '"  ', apiUrl);
-  select @curlCmd;
-  END //
-  DELIMITER ;
-  call test2('http://127.0.0.1:5000/api/user', '{\\"name\\": \\"John\\", \\"age\\": 25}')
-
-
-
-
-
-curl -X POST -H "Content-Type: application/json" -d "{\"name\": \"John\", \"age\": 25}" http://127.0.0.1:5000/api/user
-
-
+-- Exec --
+CALL public.post_data_to_api12(
+	<IN input1 character varying>, 
+	<IN input2 character varying>
+)
